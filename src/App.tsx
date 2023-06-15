978d35b86157f787fcd400072a3812bc9b8b992c
jennifer-fintlabs <jennifer@fintlabs.no>
2023-06-15 14:09:48 +0200

    testing a change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Jennifer is best :)
+          Testing contacts pod... again...
         </p>
       </header>
     </div>
